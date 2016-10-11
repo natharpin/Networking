3bf9bb44c4157206491840b1813216150ac58711
Nathan Andre Arpin <narpin@mscs.mu.edu>
2016-10-11 17:34:42 -0500

    Changed arp.h and added arptab.c to manipulate the arp table

--- network-playground/include/arp.h
+++ network-playground/include/arp.h
@@ -1,28 +1,31 @@
 
 #include <net/ip.h>
 
-struct arp_table[]{
+#define ARP_MAX 30
 
-uint_t arp_hardware_type;
-uint_t arp_protocol_type;
-uint_t arp_hardware_length;
-uint_t arp_protocol_length;
-uint_t arp_operation;
-uint_t arp_eth_source[6];
-uint_t arp_ip_source;
-uint_t arp_eth_dest[6];
-uint_t arp_ip_dest;
+struct arp_entry{
+    uchar *ipaddr;
+    uchar *mac;
+    arp_entry *next;
+}
 
+struct arp_packet{
+    uint_t arp_hardware_type;
+    uint_t arp_protocol_type;
+    uint_t arp_hardware_length;
+    uint_t arp_protocol_length;
+    uint_t arp_operation;
+    uint_t arp_eth_source[6];
+    uint_t arp_ip_source;
+    uint_t arp_eth_dest[6];
+    uint_t arp_ip_dest;
 }	
 	
 #define ARPHRD_ETHER 1
 #define ARP_OP_REQUEST 1
 #define ARP_OP_REPLY 2
 
-void arp_add(in_addr_t ip, uint8_t *mac);
+syscall arp_resolve(in_addr_t ip, uint8_t *mac);
 bool arp_remove(in_addr_t ip);
-void arp_request(char *ip_dot);
-
-
-
+syscall arp_add(uchar *ip, uchar *mac);
 
