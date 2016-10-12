ac4ddc170cef32eae2098f3dbc0161397edad8e0
Nathan Andre Arpin <narpin@mscs.mu.edu>
2016-10-11 21:12:57 -0500

    Did more stuff

--- network-playground/network/arptab.c
+++ network-playground/network/arptab.c
@@ -6,7 +6,6 @@
 int arp_count = 0;
 
 syscall arp_add(uchar *ip, uchar *mac){
-
     wait(arpadd_sem);
     struct arp_entry *current = arptab;
     if(count >= ARP_MAX){
@@ -28,5 +27,27 @@ syscall arp_add(uchar *ip, uchar *mac){
 }
 
 syscall arp_remove(in_addr_t ip){
-    return SYSERR;
+    wait(arpdelete_sem);
+    if(arp_count == 0)
+        return OK;
+    struct arp_entry *current = arptab;
+    struct arp_entry *prev = arptab;
+    char *ipaddr = (char *)malloc(sizeof(char *) * 16);
+    itoa(ip, ipaddr, 10);
+    char *buff = malloc(sizeof(char *) * 16);
+    while(current->next != NULL){
+        prev = current;
+        current = current->next;
+        if(strncmp(dot2ip(current->ipaddr, buff), ipaddr, 16)){
+            prev->next = current->next;
+            free((void *)current);
+            break;
+        } else {
+            bzero((void *)buff, sizeof(char *) * 16);
+        }
+    }
+    free((void *)ipaddr);
+    free((void *)buff);
+    signal(arpdelete_sem);
+    return OK;
 }
