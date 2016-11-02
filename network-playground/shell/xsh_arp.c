f1e22f2ba533e461df6f6ca7a8dfd43551ccedbe
Michael Colin Puiszis <mpuiszis@mscs.mu.edu>
2016-11-01 19:43:35 -0500

    pretty useless commit, doing it anyway

--- network-playground/shell/xsh_arp.c
+++ network-playground/shell/xsh_arp.c
@@ -60,25 +60,5 @@ command xsh_arp(int nargs, char *args[]){
     return OK;
 }
 
-int firstOcc(char c, char *str, int index){
-    int i;
-    for(i = index + 1; i < strlen(str); i++){
-       if(c == str[i])
-           return i;
-    }
-    return -1;
-}
-
-bool(isValidIpAddress(char *ipaddr)){
-    int i;
-    int period = 0;
-    for(i = 0; i < 3; i++){
-        if(firstOcc('.', ipaddr, period) != period && firstOcc('.', ipaddr, period) != -1)
-            period = firstOcc('.', ipaddr, period);
-        else
-            return 0;
-    }
-    return 1;
-}
 
 
